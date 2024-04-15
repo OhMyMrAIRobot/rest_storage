045aaa50350d8debd2794fa595f26e2507c44193
MrAIRobot <gfhieemobilelegends@gmail.com>
2024-04-15 19:11:08 +0300

    fix copy & move

--- server.js
+++ server.js
@@ -39,12 +39,14 @@ app.get('/files/:filename', async (req, res) => {
 
 // PUT - перезапись файла
 app.put('/files/:filename', async (req, res) => {
-    const filePath = req.body.filePath;
-    const fullPath = path.join(FILE_STORAGE_DIR, filePath);
+    const fileDir = path.join(FILE_STORAGE_DIR, path.parse(req.body.filePath).dir);
+    const fullPath = path.join(FILE_STORAGE_DIR, req.body.filePath);
     const newData = req.body.data;
-
     try {
-        await fs.access(fullPath, fs.constants.F_OK);
+        const dirExists = await fs.access(fileDir).then(() => true).catch(() => false);
+        if (!dirExists) {
+            await fs.mkdir(fileDir, { recursive: true });
+        }
         await fs.writeFile(fullPath, newData);
         res.status(200).send('File updated');
     } catch (err) {
@@ -96,16 +98,24 @@ app.delete('/files/:filename', async (req, res) => {
 // COPY - копирование файла
 app.post('/files/:filename/copy', async (req, res) => {
     const srcFilePath = path.join(FILE_STORAGE_DIR, req.body.filePath);
-    let destFilePath = path.join(FILE_STORAGE_DIR, req.body.destination);
+    let destDir = path.join(FILE_STORAGE_DIR, req.body.destination);
+    let destFilePath = path.join(destDir, req.params.filename);
 
     try {
         await fs.access(srcFilePath, fs.constants.F_OK);
-        const dirExists = await fs.access(destFilePath).then(() => true).catch(() => false);
+        const dirExists = await fs.access(destDir).then(() => true).catch(() => false);
         if (!dirExists) {
-            await fs.mkdir(destFilePath, { recursive: true });
+            await fs.mkdir(destDir, { recursive: true });
         }
 
-        destFilePath = path.join(destFilePath, req.params.filename);
+        const fileExists = await fs.access(destFilePath).then(() => true).catch(() => false);
+        if (fileExists){
+            const fileExt = path.extname(destFilePath);
+            const fileName = path.basename(destFilePath, fileExt);
+            const newFileName = `${fileName}_${(+ new Date).toString(16)}${fileExt}`;
+            destFilePath = path.join(destDir, newFileName);
+        }
+
         await fs.copyFile(srcFilePath, destFilePath);
         res.status(200).send('File copied');
     } catch (err) {
@@ -120,16 +130,24 @@ app.post('/files/:filename/copy', async (req, res) => {
 // MOVE - перемещение файла
 app.post('/files/:filename/move', async (req, res) => {
     const srcFilePath = path.join(FILE_STORAGE_DIR, req.body.filePath);
-    let destFilePath = path.join(FILE_STORAGE_DIR, req.body.destination);
+    const destDir = path.join(FILE_STORAGE_DIR, req.body.destination);
+    let destFilePath = path.join(destDir, req.params.filename);
 
     try {
         await fs.access(srcFilePath, fs.constants.F_OK);
-        const dirExists = await fs.access(destFilePath).then(() => true).catch(() => false);
+        const dirExists = await fs.access(destDir).then(() => true).catch(() => false);
         if (!dirExists) {
-            await fs.mkdir(destFilePath, { recursive: true });
+            await fs.mkdir(destDir, { recursive: true });
         }
 
-        destFilePath = path.join(destFilePath, req.params.filename);
+        const fileExists = await fs.access(destFilePath).then(() => true).catch(() => false);
+        if (fileExists){
+            const fileExt = path.extname(destFilePath);
+            const fileName = path.basename(destFilePath, fileExt);
+            const newFileName = `${fileName}_${(+ new Date).toString(16)}${fileExt}`;
+            destFilePath = path.join(destDir, newFileName);
+        }
+
         await fs.rename(srcFilePath, destFilePath);
         res.status(200).send('File moved');
     } catch (err) {
